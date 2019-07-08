3b9d09d19b4e9d15974ea3b757eeca56c2988f24
Reese Anschultz <reese-anschultz@users.noreply.github.com>
2019-07-07 23:07:52 -0700

    Added elements command

---
+++ SudokuSolverCli/UserRequestHandlers/ElementsUserRequestHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.Composition;
+
+namespace SudokuSolverCli.UserRequestHandlers
+{
+    [Export(typeof(UserRequestHandler))]
+    public class ElementsUserRequestHandler : UserRequestHandler
+    {
+        public ElementsUserRequestHandler() : base("elements")
+        {
+        }
+
+        protected override void ReallyHandleRequest(UserRequest request)
+        {
+            Console.WriteLine(string.Join(", ", Program.Board.CompleteElementSet));
+        }
+    }
+}
