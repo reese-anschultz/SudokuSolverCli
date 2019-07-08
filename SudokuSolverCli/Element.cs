587823b41f35342b7c973b5a55f09ba1c16c4b37
Reese Anschultz <reese-anschultz@users.noreply.github.com>
2019-07-07 23:06:46 -0700

    Made Element comparable

--- SudokuSolverCli/Element.cs
+++ SudokuSolverCli/Element.cs
@@ -1,12 +1,19 @@
-﻿namespace SudokuSolverCli
+﻿using System;
+
+namespace SudokuSolverCli
 {
-    public class Element
+    public class Element : IComparable
     {
         private readonly string _name;
 
         public Element(string name)
         {
             _name = name;
+        }
+
+        public int CompareTo(object obj)
+        {
+            return string.Compare(_name, obj.ToString(), StringComparison.Ordinal);
         }
 
         public override string ToString()
