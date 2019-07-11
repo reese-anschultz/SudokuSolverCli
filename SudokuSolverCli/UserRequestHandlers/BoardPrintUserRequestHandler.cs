4fcece364a0e84483b9004fe786f8a137827d973
Reese Anschultz <reese-anschultz@users.noreply.github.com>
2019-07-10 21:43:46 -0700

    Added Board Print command

---
+++ SudokuSolverCli/UserRequestHandlers/BoardPrintUserRequestHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Linq;
+
+namespace SudokuSolverCli.UserRequestHandlers
+{
+    public class BoardPrintUserRequestHandler : UserRequestHandler
+    {
+        private readonly Board _board;
+
+        private BoardPrintUserRequestHandler(Board board) : base("print")
+        {
+            _board = board;
+        }
+
+        protected override void ReallyHandleRequest(UserRequest request)
+        {
+            foreach (var rowElement in _board.CompleteElementSet)
+                Console.WriteLine(string.Join(" ", _board
+                    .CompleteElementSet
+                    .Select(columnElement => _board.GetCell(columnElement, rowElement))
+                    .Select(cell => (cell.CurrentElementSet.Count == 1 ? cell.CurrentElementSet.Single().ToString() : "."))));
+        }
+
+        [Export(typeof(UserRequestHandlerFactory<Board>))]
+        public static UserRequestHandler PrintUserRequestHandlerFactory(Board board)
+        {
+            return new BoardPrintUserRequestHandler(board);
+        }
+    }
+}
