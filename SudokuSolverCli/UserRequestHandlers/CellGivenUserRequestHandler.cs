33a91387fbbdcd38b02eae9d5d9d4771103dc696
Reese Anschultz <reese-anschultz@users.noreply.github.com>
2019-07-10 21:28:09 -0700

    Added Cell Given and removed Board Given

---
+++ SudokuSolverCli/UserRequestHandlers/CellGivenUserRequestHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.Composition;
+
+namespace SudokuSolverCli.UserRequestHandlers
+{
+    public class CellGivenUserRequestHandler : UserRequestHandler
+    {
+        private readonly Cell _cell;
+
+        private CellGivenUserRequestHandler(Cell cell) : base("given")
+        {
+            _cell = cell;
+        }
+
+        protected override void ReallyHandleRequest(UserRequest request)
+        {
+            var completeElementSet = _cell.CompleteElementSet;
+            if (!completeElementSet.TryParse(request.Argument, out var element))
+            {
+                Console.WriteLine("Expected an element name");
+                return;
+
+            }
+            _cell.RemoveElements(completeElementSet.Remove(element));
+        }
+
+        [Export(typeof(UserRequestHandlerFactory<Cell>))]
+        public static UserRequestHandler BoardHelpUserRequestHandlerFactory(Cell cell)
+        {
+            return new CellGivenUserRequestHandler(cell);
+        }
+    }
+}
