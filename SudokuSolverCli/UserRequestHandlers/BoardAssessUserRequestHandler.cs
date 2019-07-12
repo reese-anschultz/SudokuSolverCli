c60c6dbeeb1b0c7b8f6f52959453f8b20f8c209d
Reese Anschultz <reese-anschultz@users.noreply.github.com>
2019-07-11 23:36:18 -0700

    Started adding board assessment

---
+++ SudokuSolverCli/UserRequestHandlers/BoardAssessUserRequestHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+
+namespace SudokuSolverCli.UserRequestHandlers
+{
+    public class BoardAssessUserRequestHandler : UserRequestHandler
+    {
+        public delegate bool SimpleAssessor(IEnumerable<Cell> singleEnumerable);
+        private readonly BoardView _boardView;
+
+        private BoardAssessUserRequestHandler(BoardView boardView) : base("assess")
+        {
+            _boardView = boardView;
+        }
+
+        protected override void ReallyHandleRequest(UserRequest request)
+        {
+            //var assessors = _boardView.Container.GetExportedValues<SimpleAssessor>();
+        }
+
+
+        [Export(typeof(UserRequestHandlerFactory<BoardView>))]
+        public static UserRequestHandler BoardAssessUserRequestHandlerFactory(BoardView boardView)
+        {
+            return new BoardAssessUserRequestHandler(boardView);
+        }
+    }
+}
