d640fb0561326eb2e2403241fd1a80365fbe0927
Reese Anschultz <reese-anschultz@users.noreply.github.com>
2019-07-13 15:52:39 -0700

    Added region versose request handler

---
+++ SudokuSolverCli/UserRequestHandlers/RegionVerboseUserRequestHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Linq;
+
+namespace SudokuSolverCli.UserRequestHandlers
+{
+    public class RegionVerboseUserRequestHandler : UserRequestHandler
+    {
+        private readonly Region _region;
+
+        private RegionVerboseUserRequestHandler(Region region) : base("verbose")
+        {
+            _region = region;
+        }
+
+        protected override void ReallyHandleRequest(UserRequest request)
+        {
+            _region.Cells.Select(cell => cell.CurrentElementSet).ToList().ForEach(Console.WriteLine);
+        }
+
+        [Export(typeof(UserRequestHandlerFactory<Region>))]
+        public static UserRequestHandler VerboseUserRequestHandlerFactory(Region region)
+        {
+            return new RegionVerboseUserRequestHandler(region);
+        }
+    }
+}
