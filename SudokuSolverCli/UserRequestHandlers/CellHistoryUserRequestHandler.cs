4cb85076b75a87104e1f33c4b18742e1f53c3429
Reese Anschultz <reese-anschultz@users.noreply.github.com>
2019-07-13 16:03:09 -0700

    Added cell history request handler

---
+++ SudokuSolverCli/UserRequestHandlers/CellHistoryUserRequestHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Linq;
+
+namespace SudokuSolverCli.UserRequestHandlers
+{
+    public class CellHistoryUserRequestHandler : UserRequestHandler
+    {
+        private readonly Cell _cell;
+
+        private CellHistoryUserRequestHandler(Cell cell) : base("history")
+        {
+            _cell = cell;
+        }
+
+        protected override void ReallyHandleRequest(UserRequest request)
+        {
+            _cell.Changes.ToList().ForEach(change =>
+            {
+                Console.WriteLine($"Removed {change.RemovedElements} to get {change.ResultingElements}");
+                Console.WriteLine($" because {change.Why}");
+            });
+        }
+
+        [Export(typeof(UserRequestHandlerFactory<Cell>))]
+        public static UserRequestHandler CellHistoryUserRequestHandlerFactory(Cell cell)
+        {
+            return new CellHistoryUserRequestHandler(cell);
+        }
+    }
+}
