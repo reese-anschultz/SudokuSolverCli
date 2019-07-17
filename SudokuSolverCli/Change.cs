31e9b93403cb5ee29412b6b21d341978ce000fd8
Reese Anschultz <reese@anschultz.org>
2019-07-16 18:00:42 -0700

    Added Web Sudoku Evil puzzle 7686589803 and full history

--- SudokuSolverCli/Change.cs
+++ SudokuSolverCli/Change.cs
@@ -2,21 +2,21 @@
 {
     public class Change
     {
-        private readonly Cell _cell;
+        public readonly Cell Cell;
         private readonly ElementSet _removedElements;
         private readonly ElementSet _resultingElements;
         private readonly string _why;
 
         public Change(Cell cell, ElementSet removedElements, ElementSet resultingElements, string why)
         {
-            _cell = cell;
+            Cell = cell;
             _removedElements = removedElements;
             _resultingElements = resultingElements;
             _why = why;
         }
         public override string ToString()
         {
-            return $"{_cell.Location}: removed {_removedElements} to get {_resultingElements} because {_why}";
+            return $"{Cell.Location}: removed {_removedElements} to get {_resultingElements} because {_why}";
         }
     }
 }
---
+++ SudokuSolverCli/UserRequestHandlers/ApplicationFullHistoryUserRequestHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Linq;
+using SudokuSolverCli.Views;
+
+namespace SudokuSolverCli.UserRequestHandlers
+{
+    public class ApplicationFullHistoryUserRequestHandler : UserRequestHandler
+    {
+        private ApplicationFullHistoryUserRequestHandler(ApplicationView _) : base("fullhistory")
+        {
+        }
+
+        protected override void ReallyHandleRequest(UserRequest request)
+        {
+            ApplicationView.AllFinalChanges.SelectMany(change => change.Cell.Changes).ToList().ForEach(Console.WriteLine);
+        }
+
+        [Export(typeof(UserRequestHandlerFactory<ApplicationView>))]
+        public static UserRequestHandler ApplicationFullHistoryRequestHandlerFactory(ApplicationView applicationView)
+        {
+            return new ApplicationFullHistoryUserRequestHandler(applicationView);
+        }
+    }
+}
