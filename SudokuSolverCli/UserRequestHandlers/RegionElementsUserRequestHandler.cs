067b11aa06f89ec5d40edf73fd62ba95b9b4b895
Reese Anschultz <reese@anschultz.org>
2021-01-04 22:48:35 -0800

    Add board region elements command.

---
+++ SudokuSolverCli/UserRequestHandlers/RegionElementsUserRequestHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Linq;
+
+namespace SudokuSolverCli.UserRequestHandlers
+{
+    public class RegionElementsUserRequestHandler : UserRequestHandler
+    {
+        private readonly Region _region;
+
+        private RegionElementsUserRequestHandler(Region region) : base("elements")
+        {
+            _region = region;
+        }
+
+        protected override void ReallyHandleRequest(UserRequest request)
+        {
+            _region.GetElementCellLists()
+                .Select(elementWithList => $"{elementWithList.Key}: {string.Join(", ", elementWithList.Value.Select(cell => cell.Location))}")
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+
+        [Export(typeof(UserRequestHandlerFactory<Region>))]
+        public static UserRequestHandler ElementsUserRequestHandlerFactory(Region region)
+        {
+            return new RegionElementsUserRequestHandler(region);
+        }
+    }
+}
